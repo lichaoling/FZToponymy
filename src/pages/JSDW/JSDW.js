a4000c1de8a7ef05a5096545f0b02c1f11bff113
lichaoling <1191677342@qq.com>
2018-12-04 12:45:06 +0800

    样式

--- src/pages/JSDW/JSDW.js
+++ src/pages/JSDW/JSDW.js
@@ -56,9 +56,11 @@ class JSDW extends Component {
       <div className={st.JSDW}>
         <div className={st.header}>
           <div className={st.logo} />
-          <div ref={e => (this.nave = e)} className={st.nave}>
-            {this.getNavs()}
-          </div>
+          <div className={st.jz} />
+          <div className={st.user}>你好，开发商</div>
+        </div>
+        <div ref={e => (this.nave = e)} className={st.nave}>
+          <div className={st.nave_ct}>{this.getNavs()}</div>
         </div>
         <div className={st.body}>{this.getRoutes()}</div>
       </div>
